8cd7d554fa7aee7b200fa5d75ab3c78786e684c1
Nick Meinhold <nick@enspyr.co>
2025-11-18 14:38:00 +1100

    Completed 9.23

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -10,8 +10,6 @@ export enum Gender {
   Other = "other",
 }
 
-export interface Entry {}
-
 export interface Patient {
   id: string;
   name: string;
@@ -25,3 +23,49 @@ export interface Patient {
 export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
 export type PatientWithoutSsn = Omit<Patient, "ssn">;
 export type NewPatient = Omit<Patient, "id">;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
